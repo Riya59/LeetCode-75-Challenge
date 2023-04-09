2d6e571f20e8ad07b95bc7cea81fc0e8406d219e
Riya Verma <77240974+Riya59@users.noreply.github.com>
2023-04-09 23:25:17 +0530

    Create N-ary-Preorder-Traversal.java

---
+++ N-ary-Preorder-Traversal.java
@@ -0,0 +1,37 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    List<Integer> ans = new ArrayList<Integer>();
+    public void traverse(Node root){
+        if(root == null){
+            return;
+        }
+        ans.add(root.val);
+        for(Node i : root.children){
+            traverse(i);
+        }
+    }
+    public List<Integer> preorder(Node root) {
+       traverse(root);
+       return ans;
+        
+    }
+
+}
